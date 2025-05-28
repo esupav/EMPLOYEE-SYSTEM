d45c4eea3cdf7ca01afa5f0420776d3d9fa7ccbd
Gech0463 <gechgech0463@gmail.com>
2025-05-28 22:27:06 +0300

    feature-Insertion

--- feature-main.cpp
+++ feature-main.cpp
@@ -30,7 +30,113 @@ bool check_ID(const string& id) {
     }
     return true;
 }
+void insert_beg() {
+    Employee* newEmp = new Employee;
 
+    cout << "  Enter Employee Full Name: ";
+    cin.ignore();
+    getline(cin, newEmp->emp_full_name);
+    do {
+        cout << "  Enter Unique Employee ID: ";
+        cin >> newEmp->emp_id;
+        if (!check_ID(newEmp->emp_id)) cout << "  ID already exists. Try again.\n";
+    } while (!check_ID(newEmp->emp_id));
+    cout << "  Enter Employee Salary: ";
+    cin >> newEmp->emp_salary;
+    cout << "  Enter Employee Department: ";
+   cin.ignore();
+    getline(cin, newEmp->emp_department);
+    cout << "  Enter Employee phone Number: ";
+    cin >> newEmp->emp_phoneNumber;
+    newEmp->prev = NULL;
+    newEmp->next = head;
+    if (head != NULL) head->prev = newEmp;
+    else tail = newEmp;
+    head = newEmp;
+    cout << "Employee " << newEmp->emp_full_name << " inserted successfully at the beginning.\n\n";
+}
+void insert_random() {
+    int pos;
+    cout << "\nEnter the position you want to insert: ";
+    cin >> pos;
+
+    if (pos < 1||  pos > (numberOfEmp() + 1)) {
+        cout << "Invalid position!\n\n";
+        return;
+    }
+    Employee* newEmp = new Employee;
+    cout << "  Enter Employee Full Name: ";
+    cin.ignore();
+    getline(cin, newEmp->emp_full_name);
+    do {
+        cout << "  Enter Unique Employee ID: ";
+        cin >> newEmp->emp_id;
+        if (!check_ID(newEmp->emp_id)) cout << "  ID already exists. Try again.\n";
+    } while (!check_ID(newEmp->emp_id));
+    cout << "  Enter Employee Salary: ";
+    cin >> newEmp->emp_salary;
+    cout << "  Enter Employee Department: ";
+      cin.ignore();
+    getline(cin, newEmp->emp_full_name);
+    cout << "  Enter Employee phone Number: ";
+    cin >> newEmp->emp_phoneNumber;
+    newEmp->prev = NULL;
+    newEmp->next = NULL;
+    if (pos == 1) {
+        newEmp->next = head;
+        if (head != NULL) head->prev = newEmp;
+        else tail = newEmp;
+        head = newEmp;
+        cout << "Employee " << newEmp->emp_full_name << " inserted successfully at position " << pos << ".\n\n";
+        return;
+    }
+    if (pos == numberOfEmp() + 1) {
+        if (tail != NULL) {
+            tail->next = newEmp;
+            newEmp->prev = tail;
+        } else head = newEmp;
+        tail = newEmp;
+        cout << "Employee " << newEmp->emp_full_name << " inserted successfully at position " << pos << ".\n\n";
+        return;
+    }
+    Employee* temp = head;
+    for (int i = 1; i < pos - 1; i++) temp = temp->next;
+    newEmp->next = temp->next;
+    newEmp->prev = temp;
+    temp->next->prev = newEmp;
+    temp->next = newEmp;
+    cout << "Employee " << newEmp->emp_full_name << " inserted successfully at position " << pos << ".\n\n";
+}
+void insert_last() {
+    Employee* newEmp = new Employee;
+    cout << "  Enter Employee Full Name: ";
+    cin.ignore();
+    getline(cin, newEmp->emp_full_name);
+    do {
+        cout << "  Enter Unique Employee ID: ";
+        cin >> newEmp->emp_id;
+        if (!check_ID(newEmp->emp_id)) cout << "  ID already exists. Try again.\n";
+    } while (!check_ID(newEmp->emp_id));
+    cout << "  Enter Employee Salary: ";
+    cin >> newEmp->emp_salary;
+    cout << "  Enter Employee Department: ";
+     cin.ignore();
+    getline(cin, newEmp->emp_full_name);
+    cout << "  Enter Employee phone Number: ";
+    cin >> newEmp->emp_phoneNumber;
+newEmp->next = NULL;
+    if (tail == NULL) {
+        newEmp->prev = NULL;
+        head = tail = newEmp;
+
+cout << "The list was empty. Employee inserted at beginning.\n\n";
+} else {
+        newEmp->prev = tail;
+        tail->next = newEmp;
+        tail = newEmp;
+        cout << "Employee " << newEmp->emp_full_name << " inserted successfully at the end.\n\n";
+    }
+}
 void menu() {
     int choice;
     do {
@@ -48,6 +154,15 @@ void menu() {
         cin >> choice;
 switch (choice) {
             case 1: {
+                  int add;
+            cout<<" Enter 1 to insert at beginning"<<endl;
+            cout<<" Enter 2 to insert at specific position"<<endl; 
+            cout<<" Enter 3 to insert at end: ";
+                cin >> add;
+                if (add == 1) insert_beg();
+                else if (add == 2) insert_random();
+                else if (add == 3) insert_last();
+                else cout << "Invalid insert choice!\n";
                 break;
             }
             case 2: {
